247fdef69510080799604cff750ba8c6cd997034
ARCturus1 <arcturus1@yandex.ru>
2018-04-05 00:46:06 +0300

    some changes

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "root": true,
-    "env": {
-        "browser": true
-    }
+    // "extends": "standart",
+    // "root": true,
+    // "env": {
+    //     "browser": true
+    // }
 };
